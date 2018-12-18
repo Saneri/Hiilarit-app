fbb7ea5697baf1016c149ef192871c399b70aeb0
Saneri <santeri.suitiala@aalto.fi>
2018-12-18 09:57:36 +0200

    Init basic UI structure
    Create MainActivity and SearchAndAddActivity with main components
    
    Signed-off-by: Saneri <santeri.suitiala@aalto.fi>

--- app/src/main/java/com/hiilarit/appi/hiilarit_app/MainActivity.kt
+++ app/src/main/java/com/hiilarit/appi/hiilarit_app/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.hiilarit.appi.hiilarit_app
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun addButtonPressed(view: View) {
+        val intent = Intent(this, SearchAndAdd::class.java)
+        startActivity(intent)
     }
 }
