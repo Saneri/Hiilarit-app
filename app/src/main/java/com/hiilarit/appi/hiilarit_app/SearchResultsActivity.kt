8a82e6f0db186c3eaf0c72de4f504a728d06767b
Saneri <santeri.suitiala@aalto.fi>
2019-01-23 19:30:02 +0200

    Add JSONrequest to talk with Fineli API
    Signed-off-by: Saneri <santeri.suitiala@aalto.fi>

--- app/src/main/java/com/hiilarit/appi/hiilarit_app/SearchResultsActivity.kt
+++ app/src/main/java/com/hiilarit/appi/hiilarit_app/SearchResultsActivity.kt
@@ -4,6 +4,11 @@ import android.app.SearchManager
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 
 class SearchResultsActivity : AppCompatActivity() {
 
@@ -11,8 +16,8 @@ class SearchResultsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_results)
         handleIntent(intent)
-    }
 
+    }
 
     override fun onNewIntent(intent: Intent) {
         handleIntent(intent)
@@ -21,8 +26,27 @@ class SearchResultsActivity : AppCompatActivity() {
     private fun handleIntent(intent: Intent) {
 
         if (Intent.ACTION_SEARCH == intent.action) {
+            val queue = Volley.newRequestQueue(this)
             val query = intent.getStringExtra(SearchManager.QUERY)
-            //use the query to search your data somehow
+            val url = "https://fineli.fi/fineli/api/v1/foods?q="
+            val textView = findViewById(R.id.textView2) as TextView
+
+            val jsonObjectRequest = JsonObjectRequest(
+                Request.Method.GET,
+                "$url$query",
+                null,
+                Response.Listener { response ->
+                    textView.text = "Response: %s".format("Success!")
+                    // Never goes here for unknown reason
+                },
+                Response.ErrorListener { error ->
+                    // TODO: Handle error
+                    textView.text = "Response: %s".format(error.message)
+                    // atm error message gives response
+                }
+            )
+            queue.add(jsonObjectRequest)
+
         }
     }
 }
